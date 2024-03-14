2f17b28e0f218f80f39348c3ecd500d2b9dfb554
Shekhar Ashok Mohite <122733304+shekharma@users.noreply.github.com>
2024-03-14 22:26:17 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,186 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+## 1. Create Data
+
+# Set the mean and covariance
+mean1 = [0, 0]
+mean2 = [2, 0]
+cov1 = [[1, .7], [.7, 1]]
+cov2 = [[.5, .4], [.4, .5]]
+
+# Generate data from the mean and covariance
+data1 = np.random.multivariate_normal(mean1, cov1, size=1000)
+data2 = np.random.multivariate_normal(mean2, cov2, size=1000)
+
+## 2. Plot Data
+
+plt.figure(figsize=(10,6))
+
+plt.scatter(data1[:,0],data1[:,1])
+plt.scatter(data2[:,0],data2[:,1])
+
+sns.kdeplot(x=data1[:, 0], y=data1[:, 1], levels=20, color='k', alpha=0.2)
+sns.kdeplot(x=data2[:, 0], y=data2[:, 1], levels=20, color='k', alpha=0.2)
+
+plt.grid(False)
+plt.show()
+
+import numpy as np
+
+n_samples = 100
+mu1, sigma1 = -5, 1.2 
+mu2, sigma2 = 5, 1.8 
+mu3, sigma3 = 0, 1.6 
+
+x1 = np.random.normal(loc = mu1, scale = np.sqrt(sigma1), size = n_samples)
+x2 = np.random.normal(loc = mu2, scale = np.sqrt(sigma2), size = n_samples)
+x3 = np.random.normal(loc = mu3, scale = np.sqrt(sigma3), size = n_samples)
+
+X = np.concatenate((x1,x2,x3))
+
+from scipy.stats import norm
+
+def plot_pdf(mu,sigma,label,alpha=0.5,linestyle='k--',density=True):
+    """
+    Plot 1-D data and its PDF curve.
+
+    """
+    # Compute the mean and standard deviation of the data
+
+    # Plot the data
+    
+    X = norm.rvs(mu, sigma,size=1000)
+    
+    plt.hist(X, bins=50, density=density, alpha=alpha,label=label)
+
+    # Plot the PDF
+    x = np.linspace(X.min(), X.max(), 1000)
+    y = norm.pdf(x, mu, sigma)
+    plt.plot(x, y, linestyle)
+
+plot_pdf(mu1,sigma1,label=r"$\mu={} \ ; \ \sigma={}$".format(mu1,sigma1))
+plot_pdf(mu2,sigma2,label=r"$\mu={} \ ; \ \sigma={}$".format(mu2,sigma2))
+plot_pdf(mu3,sigma3,label=r"$\mu={} \ ; \ \sigma={}$".format(mu3,sigma3))
+plt.legend()
+plt.show()
+
+## 1. Initialize Mean, Covariance, and weights
+
+def random_init(n_compenents):
+    
+    """Initialize means, weights and variance randomly 
+      and plot the initialization
+    """
+    
+    pi = np.ones((n_compenents)) / n_compenents
+    means = np.random.choice(X, n_compenents)
+    variances = np.random.random_sample(size=n_compenents)
+
+    plot_pdf(means[0],variances[0],'Random Init 01')
+    plot_pdf(means[1],variances[1],'Random Init 02')
+    plot_pdf(means[2],variances[2],'Random Init 03')
+    
+    plt.legend()
+    plt.show()
+    
+    return means,variances,pi
+
+## 2. Expectation Step
+
+def step_expectation(X,n_components,means,variances):
+    """E Step
+    
+    Parameters
+    ----------
+    X : array-like, shape (n_samples,)
+        The data.
+    n_components : int
+        The number of clusters
+    means : array-like, shape (n_components,)
+        The means of each mixture component.
+    variances : array-like, shape (n_components,)
+        The variances of each mixture component.
+        
+    Returns
+    -------
+    weights : array-like, shape (n_components,n_samples)
+    """
+    weights = np.zeros((n_components,len(X)))
+    for j in range(n_components):
+        weights[j,:] = norm(loc=means[j],scale=np.sqrt(variances[j])).pdf(X)
+    return weights
+
+## 3. Maximization Step
+
+def step_maximization(X,weights,means,variances,n_compenents,pi):
+    """M Step
+    
+    Parameters
+    ----------
+    X : array-like, shape (n_samples,)
+        The data.
+    weights : array-like, shape (n_components,n_samples)
+        initilized weights array
+    means : array-like, shape (n_components,)
+        The means of each mixture component.
+    variances : array-like, shape (n_components,)
+        The variances of each mixture component.
+    n_components : int
+        The number of clusters
+    pi: array-like (n_components,)
+        mixture component weights
+        
+    Returns
+    -------
+    means : array-like, shape (n_components,)
+        The means of each mixture component.
+    variances : array-like, shape (n_components,)
+        The variances of each mixture component.
+    """
+    r = []
+    for j in range(n_compenents):  
+
+        r.append((weights[j] * pi[j]) / (np.sum([weights[i] * pi[i] for i in range(n_compenents)], axis=0)))
+
+        #5th equation above
+        means[j] = np.sum(r[j] * X) / (np.sum(r[j]))
+        
+        #6th equation above
+        variances[j] = np.sum(r[j] * np.square(X - means[j])) / (np.sum(r[j]))
+        
+        #4th equation above
+        pi[j] = np.mean(r[j])
+
+    return variances,means,pi
+
+
+## 4. Training
+
+def train_gmm(data,n_compenents=3,n_steps=50, plot_intermediate_steps_flag=True):
+    """ Training step of the GMM model
+    
+    Parameters
+    ----------
+    data : array-like, shape (n_samples,)
+        The data.
+    n_components : int
+        The number of clusters
+    n_steps: int
+        number of iterations to run
+    """
+    
+    #intilize model parameters at the start
+    means,variances,pi = random_init(n_compenents)
+
+    for step in range(n_steps):
+        #perform E step
+        weights = step_expectation(data,n_compenents,means,variances)
+        #perform M step
+        variances,means,pi = step_maximization(X, weights, means, variances, n_compenents, pi)
+
+    plot_pdf(means,variances)
+
+train_gmm(X,n_compenents=3,n_steps=50, plot_intermediate_steps_flag=True)
 
